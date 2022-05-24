9ff858e775b81362a62e91f34b711e20598c6f73
RamiroMM21 <105456306+RamiroMM21@users.noreply.github.com>
2022-05-24 11:21:39 -0700

    try catch pract
    bgh

---
+++ trycatch-practica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatch_practica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Delaración de variables
+            int Numero;
+
+          try
+            {
+                Console.Write("Ingrese un número entero: ");
+                Numero = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nEl cuadrado del número entero es: " + Math.Pow(Numero, 2));
+            }
+            catch(OverflowException s)
+            {
+                Console.WriteLine(s.Message);
+            }
+            catch(FormatException s)
+            {
+                Console.WriteLine(s.Message);
+            }
+            Console.WriteLine("\nPresione ENTER para salir");
+            Console.ReadKey();
+        }
+    }
+}
